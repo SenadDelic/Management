fcc2ad97aabd77fbfb440989548673789ecde2b3
SenadDelic <dellic.senad@gmail.com>
2020-06-17 15:49:51 +0200

    Connection is done,hope it works :3

--- src/dao/Constants.java
+++ src/dao/Constants.java
@@ -1,4 +1,7 @@
 package dao;
 
 public class Constants {
+    public static final String DB_NAME = "Management";
+    public static final String CONNECTION_STRING = "jdbc:mysql://localhost:3306/" + DB_NAME;
+    public static final String TIME_ZONE_ERROR = "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
 }
