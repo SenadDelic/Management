2c9e3124223f25fd4ac1b4e13a2421e73637f87a
SenadDelic <dellic.senad@gmail.com>
2020-06-17 16:21:17 +0200

    Working on userDb crud operations

--- src/dao/Constants.java
+++ src/dao/Constants.java
@@ -1,7 +1,15 @@
 package dao;
 
 public class Constants {
-    public static final String DB_NAME = "Management";
-    public static final String CONNECTION_STRING = "jdbc:mysql://localhost:3306/" + DB_NAME;
-    public static final String TIME_ZONE_ERROR = "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+    protected static final String DB_NAME = "Management";
+    protected static final String CONNECTION_STRING = "jdbc:mysql://localhost:3306/" + DB_NAME;
+    protected static final String TIME_ZONE_ERROR = "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+
+    // Users
+    protected static final String INSERT_USERS_SQL = "INSERT INTO users" + "  (name, email, country) VALUES "
+            + " (?, ?, ?);";
+    protected static final String SELECT_USER_BY_ID = "select id,name,email,country from users where id = ?";
+    protected static final String SELECT_ALL_USERS = "select * from users";
+    protected static final String DELETE_USERS_SQL = "delete from users where id = ?;";
+    protected static final String UPDATE_USERS_SQL = "update users set name = ?, email= ?, country = ? where id = ?;";
 }
--- src/dao/UserDb.java
+++ src/dao/UserDb.java
@@ -1,4 +1,94 @@
 package dao;
 
+import model.User;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * CRUD database operations for table users
+ */
+
 public class UserDb {
+
+    public void insertUser(User user) {
+        try (PreparedStatement preparedStatement = ConnectionManagement.getInstance().getConnection().prepareStatement(Constants.INSERT_USERS_SQL)) {
+            preparedStatement.setString(1, user.getName());
+            preparedStatement.setString(2, user.getEmail());
+            preparedStatement.setString(3, user.getCountry());
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public User selectUser(int id) {
+        User user = null;
+
+        try (PreparedStatement preparedStatement = ConnectionManagement.getInstance().getConnection().prepareStatement(Constants.SELECT_USER_BY_ID)) {
+            preparedStatement.setInt(1, id);
+
+            ResultSet rs = preparedStatement.executeQuery();
+            while (rs.next()) {
+                String name = rs.getString("name");
+                String email = rs.getString("email");
+                String country = rs.getString("country");
+                user = new User(id, name, email, country);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return user;
+    }
+
+    public List<User> printUsers() {
+        List<User> users = new ArrayList<>();
+
+        try (PreparedStatement preparedStatement = ConnectionManagement.getInstance().getConnection().prepareStatement(Constants.SELECT_ALL_USERS)) {
+            ResultSet rs = preparedStatement.executeQuery();
+
+            while (rs.next()) {
+                int id = rs.getInt(1);
+                String name = rs.getString("name");
+                String email = rs.getString("email");
+                String country = rs.getString("country");
+                users.add(new User(id, name, email, country));
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return users;
+    }
+
+    // delete user by id
+    public boolean deleteUser(int id) {
+        boolean update = false;
+
+        try (PreparedStatement preparedStatement = ConnectionManagement.getInstance().getConnection().prepareStatement(Constants.DELETE_USERS_SQL)) {
+            preparedStatement.setInt(1, id);
+            update = preparedStatement.executeUpdate() > 0;
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return update;
+    }
+
+    public boolean updateUser(User user) {
+        boolean rowUpdated = false;
+        try (PreparedStatement statement = ConnectionManagement.getInstance().getConnection().prepareStatement(Constants.UPDATE_USERS_SQL);) {
+            statement.setString(1, user.getName());
+            statement.setString(2, user.getEmail());
+            statement.setString(3, user.getCountry());
+            statement.setInt(4, user.getId());
+
+            rowUpdated = statement.executeUpdate() > 0;
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return rowUpdated;
+    }
 }
